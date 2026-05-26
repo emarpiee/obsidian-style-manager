783a3c84508edea3309e5c203b9c823d72e507d9
emarpiee <mrkrynp@gmail.com>
2026-05-27 04:32:19 +0800

    style: reformat .eslintrc.js and relax unused-vars for tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,61 +1,59 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-    jest: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': 'error',
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'error',
-    '@typescript-eslint/no-this-alias': 'off',
-    '@typescript-eslint/no-inferrable-types': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    'react/no-unescaped-entities': 'off',
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-        caughtErrorsIgnorePattern: '^_',
-      },
-    ],
-    'linebreak-style': ['error', 'unix'],
-    indent: 'off',
-    quotes: 'off',
-  },
-  overrides: [
-    {
-      // Test files and mocks: relax any and explicit return types
-      files: ['src/test/**/*.ts'],
-      rules: {
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-      },
-    },
-  ],
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+		jest: true,
+	},
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {
+		'@typescript-eslint/explicit-function-return-type': 'error',
+		'@typescript-eslint/no-explicit-any': 'error',
+		'@typescript-eslint/no-empty-function': 'off',
+		'@typescript-eslint/no-var-requires': 'off',
+		'@typescript-eslint/no-use-before-define': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'error',
+		'@typescript-eslint/no-this-alias': 'off',
+		'@typescript-eslint/no-inferrable-types': 'off',
+		'@typescript-eslint/ban-ts-comment': 'off',
+		'react/no-unescaped-entities': 'off',
+		'react/prop-types': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{
+				argsIgnorePattern: '^_',
+				varsIgnorePattern: '^_',
+				caughtErrorsIgnorePattern: '^_',
+			},
+		],
+		'linebreak-style': ['error', 'unix'],
+		indent: 'off',
+		quotes: 'off',
+	},
+	overrides: [
+		{
+			// Test files and mocks: relax any and explicit return types
+			files: ['src/test/**/*.ts'],
+			rules: {
+				'@typescript-eslint/no-explicit-any': 'off',
+				'@typescript-eslint/explicit-function-return-type': 'off',
+				'@typescript-eslint/explicit-module-boundary-types': 'off',
+				'@typescript-eslint/no-unused-vars': 'off',
+			},
+		},
+	],
 };
