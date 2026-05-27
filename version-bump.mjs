9b9621299acef1a7836f6fba9206d8ffb3a2a268
emarpiee <mrkrynp@gmail.com>
2026-05-27 10:30:20 +0800

    chore: update descriptions and refactor version-bump script

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,14 +1,15 @@
-import { readFileSync, writeFileSync } from "fs";
+import { readFileSync, writeFileSync } from 'fs';
+import { URL } from 'url';
 
-const targetVersion = process.env.npm_package_version;
-
-// read minAppVersion from manifest.json and bump version to target version
-let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
+let manifest = JSON.parse(readFileSync('manifest.json', 'utf8'));
+let pkg = JSON.parse(readFileSync('package.json', 'utf8'));
 const { minAppVersion } = manifest;
-manifest.version = targetVersion;
-writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t") + '\n');
+
+manifest.version = pkg.version;
+manifest.description = pkg.description;
+writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t') + '\n');
 
 // update versions.json with target version and minAppVersion from manifest.json
-let versions = JSON.parse(readFileSync("versions.json", "utf8"));
-versions[targetVersion] = minAppVersion;
-writeFileSync("versions.json", JSON.stringify(versions, null, "\t") + '\n');
+let versions = JSON.parse(readFileSync('versions.json', 'utf8'));
+versions[pkg.version] = minAppVersion;
+writeFileSync('versions.json', JSON.stringify(versions, null, '\t') + '\n');
